b0e223bcd84124c91e72b815d66f0e215f1a5294
Tayari <tayarimarwen@gmail.com>
2021-06-02 21:32:40 +0100

    make it better

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require ('express')
 const connectDB = require ('./helpers/ConnectDB')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
 //connect to DataBase 
 connectDB() 
-
 
 /*app.options('*', cors());
 middlewares  */
@@ -21,6 +21,16 @@ app.use("/update" , require('./routes/update'))
 app.use("/announce" , require('./routes/announce'))
 app.use("/users" , require('./routes/usersList'))
 app.use("/comment" , require('./routes/comment'))
+
+//serve a static assets if in production 
+if(process.env.NODE_ENV ==='production') {
+    //set static folder 
+    app.use(express.static('client/build')); 
+
+    app.get('*',(req,res) =>{
+      res.sendFile(path.resolve(__dirname,'client','bulid','index.html'))
+    })
+}
 
 const PORT  = process.env.PORT || 5000     
 app.listen(PORT, ()=> console.log(`The server is running on PORT : ${PORT}`))     
